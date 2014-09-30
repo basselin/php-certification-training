9ec02177557e9ee54806b7ef73ba89a5024372db
Benoit Asselin <contact@ab-d.fr>
2014-09-30 14:06:16 +0200

    Create 0131.php

---
+++ Questions/0131.php
@@ -0,0 +1,21 @@
+<?php
+
+// Consider the following PHP script fragment:
+
+$title = $dom->createElement('title');
+$node = ????????
+
+$title->appendChild($node);
+$head->appendChild($title);
+
+// What should ??????? be replaced with to add a <title> node with the value of Hello, World!
+
+/*
+
+1) $dom->createTextNode("Hello, World");
+2) $dom->appendElement($title, "text", "Hello, world!");
+3) $dom->appendTextNode($title, "Hello, World!");
+4) $dom->createElement('text', "Hello, World");
+5) None of the above 
+
+*/
