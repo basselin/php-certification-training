8adf4258db24243fdd3ecc0b954eaa76e66f06ce
Benoit Asselin <contact@ab-d.fr>
2014-09-30 14:02:33 +0200

    Create 0132.php

---
+++ Questions/0132.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+
+//  When working with SimpleXML in PHP 5, the four basic rules on how the XML document is accessed are which of the following?
+
+1) Element namespaces are denoted by the 'namespace' attribute          
+2) converting an element to a string denotes text data                  OK
+3) Non-numeric indexes are element attributes                           OK
+4) Numeric indexes are elements                                         OK
+5) Properties denote element iterators                                  OK
+
+*/
