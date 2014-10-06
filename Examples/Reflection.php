c1a82fcce3e932b96adabd99c3a766af51f9d46b
Benoit Asselin <contact@ab-d.fr>
2014-10-06 13:55:25 +0200

    Create Reflection.php

---
+++ Examples/Reflection.php
@@ -0,0 +1,58 @@
+<pre><?php
+/**
+ * @see http://php.net/manual/en/class.reflection.php
+ * @version PHP 5+
+ */
+
+class Person
+{
+    public $name;
+    protected $spouse;
+    private $password;
+
+    /**
+     * My constructor
+     * @param string $name
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setSpouse(Person $spouse)
+    {
+        $this->spouse = $spouse;
+    }
+
+    private function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
+
+$reflectionClass = new ReflectionClass('Person');
+echo Reflection::export($reflectionClass, true);
+
+$firstMethod = $reflectionClass->getMethods()[0];
+$firstProperty = $reflectionClass->getProperties()[0];
+
+var_dump(
+    $reflectionClass->getMethods(), // ReflectionMethod[]
+    $reflectionClass->hasMethod('getName'),
+    $firstMethod->getDocComment(),
+    $firstMethod->getParameters(), // ReflectionParameter[]
+    //
+    $reflectionClass->getProperties(), // ReflectionProperty[]
+    $firstProperty->isPublic()
+    );
+
+echo '<hr />';
+
+ReflectionObject::export(new Person('Reflection'));
+
+?></pre>
