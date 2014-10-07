7e4d91ec5155d2437b4437df8c75c3d9c1590e94
Benoit Asselin <contact@ab-d.fr>
2014-10-07 13:33:40 +0200

    Create Exception.php

---
+++ Documentation/Exception.php
@@ -0,0 +1,27 @@
+<?php
+
+class Exception
+{
+    protected $message = 'Unknow exception';  // exception message
+    private   $string;                        // __toString cache
+    protected $code = 0;                      // user defined exception code
+    protected $file;                          // source filename of exception
+    protected $line;                          // source line of exception
+    private   $trace;                         // backtrace
+    private   $previous;                      // previous exception if nested exception  PHP 5.3+
+
+    public function __construct($message = null, $code = 0, Exception $previous = null);
+
+    final private function __clone();         // Inhibits cloning of exceptions
+
+    final public function getMessage();       // message of exception
+    final public function getCode();          // code of exception
+    final public function getFile();          // source filename
+    final public function getLine();          // source line
+    final public function getTrace();         // an array of the backtrace()
+    final public function getPrevious();      // previous exception  PHP 5.3+
+    final public function getTraceAsString(); // formatted string of trace
+
+    public function __toString();             // formatted string for display
+}
+
