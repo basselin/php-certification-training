fc5b3612000289aaf2b98bcf5a504fb06b77db41
Benoit Asselin <contact@ab-d.fr>
2014-10-09 23:08:07 +0200

    Example

--- Documentation/TODO.php
+++ Documentation/TODO.php
@@ -1,5 +1,6 @@
 <?php
 
+// http://fr2.php.net/manual/fr/ini.list.php
 // http://php.net/manual/fr/ref.hash.php
 // http://php.net/manual/en/book.stream.php
 // http://php.net/manual/en/ref.errorfunc.php
