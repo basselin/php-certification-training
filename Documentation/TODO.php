94e5e418ed4b5ec32205b90dd2f30aa175615ba0
Benoit Asselin <contact@ab-d.fr>
2014-10-12 17:13:17 +0200

    read

--- Documentation/TODO.php
+++ Documentation/TODO.php
@@ -5,5 +5,11 @@
 // http://php.net/manual/en/book.stream.php
 // http://php.net/manual/en/ref.errorfunc.php
 // http://php.net/manual/en/book.simplexml.php
+// http://php.net/manual/en/class.domdocument.php
 // http://php.net/manual/en/book.mysqli.php
-// http://php.net/manual/en/ref.math.php
+// http://php.net/manual/en/book.pdo.php
+// http://php.net/manual/en/book.spl.php
+// http://php.net/manual/en/book.ctype.php
+
+// http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+
