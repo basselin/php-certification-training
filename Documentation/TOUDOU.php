e3488078fcdbe11138a45ed76a041f16232c204e
Benoit Asselin <contact@ab-d.fr>
2014-10-13 21:56:10 +0200

    TODO2

---
+++ Documentation/TOUDOU.php
@@ -0,0 +1,33 @@
+<?php
+
+// http://php.net/manual/en/language.types.string.php HEREDOC
+// http://php.net/manual/en/language.types.array.php
+// http://php.net/manual/en/language.functions.php
+// http://php.net/manual/en/language.operators.increment.php
+// http://php.net/manual/en/language.references.php
+// http://php.net/manual/en/language.namespaces.php
+// http://php.net/manual/en/language.oop5.final.php
+// http://php.net/manual/en/language.oop5.interfaces.php
+// http://php.net/manual/en/language.oop5.traits.php
+// http://php.net/manual/en/language.oop5.typehinting.php
+// http://php.net/manual/en/language.oop5.late-static-bindings.php
+// http://php.net/manual/en/language.variables.superglobals.php
+// http://php.net/manual/en/features.file-upload.php
+// http://php.net/manual/en/ref.filesystem.php
+// http://php.net/manual/en/ref.pcre.php
+// http://php.net/manual/en/book.session.php
+// http://php.net/manual/en/book.stream.php
+// http://php.net/manual/en/book.simplexml.php
+// http://php.net/manual/en/book.spl.php
+
+// http://php.net/manual/en/reserved.interfaces.php
+// http://php.net/manual/en/class.closure.php
+
+// http://en.wikipedia.org/wiki/XML_schema
+// http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+// http://en.wikipedia.org/wiki/Join_(SQL)
+// http://en.wikipedia.org/wiki/Cross-site_scripting
+// http://en.wikipedia.org/wiki/Cross-site_request_forgery
+// http://php.net/manual/en/security.database.sql-injection.php
+// http://php.net/manual/en/function.htmlentities.php & +
+
